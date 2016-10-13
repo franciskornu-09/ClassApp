df220a5e060c2daaac541b9d71d074bfef09ca1b
fkornu <franciskornu@gmail.com>
2016-10-13 11:00:10 +0000

    new changes

---
+++ myscript1.js
@@ -0,0 +1,25 @@
+(function(){
+		document.addEventListener('deviceready',onDeviceReady.bind(this),false);
+		var pictureSource;
+		var destinationType;
+
+		function onDeviceReady(){
+			pictureSource=navigator.camera.PictureSourceType;
+			destination=navigator.camera.DestinationType;
+
+			document.getElementById("cameraPhoto").onclick = function(){
+				navigator.camera.getPicture(onPhotoDataSuccces, onFail,{
+					quality:50,
+					
+					destinationType:destinationType.DATA_URL
+				});
+			}	
+		};
+
+		function onPhotoDataSuccces(imageData){
+			var smallImage = document.getElementById('smallImage');
+			smallImage.style.display='block';
+			smallImage.src="data.image/jpeg;base64,"+imageData;
+		}
+		})
+		();
